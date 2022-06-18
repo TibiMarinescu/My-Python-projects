af0c4f11da708cdd67eb7d9fb451e84e79614cc6
TibiMarinescu <64827320+TibiMarinescu@users.noreply.github.com>
2022-06-18 17:56:56 +0300

    Update Library.py

--- Library.py
+++ Library.py
@@ -4,15 +4,18 @@ import jsonpath
 import openpyxl
 
 class Common:
-  def fetch_row_count(self, FileNamePath, SheetName):
+   
+  def __init__(self):
+    global vk
+    global sh
     vk = openpyxl.load_workbook(FileNamePath)
     sh = vk[SheetName]
+      
+  def fetch_row_count(self, FileNamePath, SheetName):
     max_rows = sh.max_row
     return max_rows
   
   
   def fetch_column_count(self, FileNamePath,SheetName):
-    vk = openpyxl.load_workbook(FileNamePath)
-    sh = vk[SheetName]
     max_columns = sh.max_column
     return max_columns
