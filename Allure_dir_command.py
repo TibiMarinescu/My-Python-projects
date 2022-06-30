aa127f6dccfb4e8b4bcb6f6457f7a59c51406644
TibiMarinescu <64827320+TibiMarinescu@users.noreply.github.com>
2022-06-30 19:56:31 +0300

    Update Allure_dir_command.py

--- Allure_dir_command.py
+++ Allure_dir_command.py
@@ -1,2 +1,3 @@
 #Allure command in open terminal
 # pytest --alluredir C:\Users\ungur\PycharmProjects\APIAutomation\Reports TestCases
+# allure serve C:\Users\ungur\PycharmProjects\APIAutomation\Reports
